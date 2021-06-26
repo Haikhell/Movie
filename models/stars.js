9be07650d8e454b32a4e8eb7a25572f6d3422fec
Nazarii-Kozinets <kozinets.nazarii@lll.kpi.ua>
2021-06-27 01:47:05 +0300

    Clear

--- models/stars.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const starSchema = new mongoose.Schema({
-    fullname: { type: String, required: false },
-
-}, { collection: 'stars' });
-
-module.exports = mongoose.model('Star', starSchema);
