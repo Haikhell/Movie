860654d1791110ecfad37d9d9a611aafb8c4ea4c
Haikhell <40888624+Haikhell@users.noreply.github.com>
2021-08-29 19:04:36 +0300

    Update media.js

--- controllers/media.js
+++ controllers/media.js
@@ -1,5 +1,5 @@
 const MediaRepository = require('./../repositories/mediaRepository');
-
+// TODO Divide into two different files
 module.exports = {
     async deleteMediaFile(url, next) {
         try {
@@ -8,6 +8,7 @@ module.exports = {
             }
         } catch (err) {
             if (err instanceof myError) return next(err);
+            // TODO superfluous else
             else return next(new myError(500, err.message));
         }
     },
@@ -18,10 +19,13 @@ module.exports = {
                 const media_url = await MediaRepository.addMedia(req.files.textFile.data)
                 return media_url.url
             }
+            // TODO superfluous else
             else if (fileUrl) return fileUrl
+            // TODO superfluous else
             else return 'ERROR Media.js 22'
         } catch (err) {
             if (err instanceof myError) return next(err);
+            // TODO superfluous else
             else return next(new myError(500, err.message));
         }
     },
